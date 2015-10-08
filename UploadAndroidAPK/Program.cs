ae5c5e85cf4381673a295984e32267bd5300b429
James Baxter <j.w.baxter@gmail.com>
2015-10-08 19:59:29 +0100

    Pulled out oauth access token lookup

--- UploadAndroidAPK/Program.cs
+++ UploadAndroidAPK/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
-using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using RestSharp;
 
 namespace UploadAndroidAPK
@@ -11,19 +9,18 @@ namespace UploadAndroidAPK
     {
         static void Main(string[] args)
         {
-            if (args.Length != 5)
+            if (args.Length != 2)
             {
-                Console.WriteLine("Required args: <application_id> <version> <client_id> <client_secret> <refresh_token>");
+                Console.WriteLine("Required args: <application_id> <version>");
                 return;
             }
 
-            Console.WriteLine("Preparing to upload APK");
             var applicationId = args[0];
             var version = int.Parse(args[1]);
-            var clientId = args[2];
-            var clientSecret = args[3];
-            var refreshToken = args[4];
-            var accessToken = GetAccessToken(clientId, clientSecret, refreshToken);
+            var accessToken = Console.ReadLine();
+
+            Console.WriteLine("Preparing to upload APK");
+            
             var restClient = new RestClient("https://www.googleapis.com");
             var editId = CreateEdit(restClient, applicationId, accessToken);
             UploadApk(restClient, accessToken, applicationId, editId);
@@ -32,19 +29,6 @@ namespace UploadAndroidAPK
 
             if (Debugger.IsAttached)
                 Console.ReadLine();
-        }
-        
-        private static string GetAccessToken(string clientId, string clientSecret, string refreshToken)
-        {
-            var client = new RestClient("https://accounts.google.com/o/oauth2/token");
-            var request = new RestRequest(Method.POST);
-            request.AddParameter("grant_type", "refresh_token");
-            request.AddParameter("client_id", clientId);
-            request.AddParameter("client_secret", clientSecret);
-            request.AddParameter("refresh_token", refreshToken);
-            var response = client.Execute<RefreshTokenResponse>(request);
-            CheckResponse(response);
-            return response.Data.AccessToken;
         }
 
         private static string CreateEdit(IRestClient restClient, string applicationId, string accessToken)
